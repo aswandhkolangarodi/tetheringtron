3559581b9cdff7b8370a7d882a64cde4e7a5b7b0
aswandhkolangarodi <aswandhk2000@gmail.com>
2022-08-27 10:15:25 +0530

    aa

--- trx/settings.py
+++ trx/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config()
+SECRET_KEY = 'django-insecure-k6up6g1eb0av&k=be#^@x0d)hytb#_d2gt0&^$0fwa2!zrg69#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
